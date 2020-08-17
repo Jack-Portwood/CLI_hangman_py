c3b728c33dfc5dd6feb044100a9e8f59f02fd08e
Jack Portwood <Jack.Portwood14@gmail.com>
2020-08-17 13:34:56 +0100

    conditional logic for letter guess

---
+++ hangman.py
@@ -0,0 +1,26 @@
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    gussed_words = []
+    tries = 6
+    print("Lets play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+        
+        elif len(guess) == len(word) and guess.isalpha():
+
+        else:
+            print ("Nae luck try again...")
